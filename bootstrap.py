e660e772fc116e3a993cc64f5a91e0d840d7cde8
GoLden1EaGle <priyanshu.sharma@opensenselabs.com>
2025-04-22 04:35:30 +0530

    made repository file contents and bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -1,22 +1,22 @@
-# from server_runtime.lifecycle_server import ServerLifeCycleServer
-# from server_runtime.registry_server import RegistryServer
+from server_runtime.lifecycle_server import ServerLifeCycleServer
+from server_runtime.registry_server import RegistryServer
 from server_runtime.repository_server import RepositoryServer
-# from server_runtime.logging_server import LoggingServer
+from server_runtime.logging_server import LoggingServer
 
 # TODO: start load balancer
 
-# reg_server = RegistryServer("./registry")
+reg_server = RegistryServer("./registry")
 
-# reg_pid = reg_server.start()
-#
+reg_pid = reg_server.start()
+
 repo_server = RepositoryServer("./repository")
 
 repo_pid = repo_server.start()
-#
-# life_server = ServerLifeCycleServer("./server_lifecycle_manager")
-#
-# life_server_pid = life_server.start()
 
-# logging_server = LoggingServer("./logging")
+life_server = ServerLifeCycleServer("./server_lifecycle_manager")
 
-# logging_server_pid = logging_server.start()
+life_server_pid = life_server.start()
+
+logging_server = LoggingServer("./logging")
+
+logging_server_pid = logging_server.start()
