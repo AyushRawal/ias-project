568fc4abfd58b32491ac0697ca995e0d0bb1d34d
GoLden1EaGle <priyanshu.sharma@opensenselabs.com>
2025-04-22 04:50:24 +0530

    made load_balancer and integrated

--- bootstrap.py
+++ bootstrap.py
@@ -2,6 +2,7 @@ from server_runtime.lifecycle_server import ServerLifeCycleServer
 from server_runtime.registry_server import RegistryServer
 from server_runtime.repository_server import RepositoryServer
 from server_runtime.logging_server import LoggingServer
+from load_balancer_server import LoadBalancerServer
 
 # TODO: start load balancer
 
@@ -20,3 +21,7 @@ life_server_pid = life_server.start()
 logging_server = LoggingServer("./logging")
 
 logging_server_pid = logging_server.start()
+
+lb_server = LoadBalancerServer("./load_balancer")
+
+lb_server_pid = lb_server.start()
